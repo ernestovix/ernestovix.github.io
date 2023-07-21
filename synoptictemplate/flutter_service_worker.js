faaa3f153ddb33cacce4f6a4126b767a5ab89854
ernestovix <139761498+ernestovix@users.noreply.github.com>
2023-07-21 12:22:20 +0100

    v3

--- synoptictemplate/flutter_service_worker.js
+++ synoptictemplate/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "0634103797dca6e0937a27da12fa0873",
 "icons/Icon-maskable-192.png": "0634103797dca6e0937a27da12fa0873",
 "icons/Icon-maskable-512.png": "0634103797dca6e0937a27da12fa0873",
-"index.html": "93006f09b16413caf28c58e41544a735",
-"/": "93006f09b16413caf28c58e41544a735",
-"main.dart.js": "2c1277cea695f5032baf5d1851fcaee0",
+"index.html": "11196c4ee98d5dfdc410f2009072d931",
+"/": "11196c4ee98d5dfdc410f2009072d931",
+"main.dart.js": "f45c9f527f46961adc837dfef9c2958d",
 "manifest.json": "b1f58066053ed02190c4d4f07de60b90",
 "version.json": "845fc90666e54ffac682273f871254cd"};
 // The application shell files that are downloaded before a service worker can
